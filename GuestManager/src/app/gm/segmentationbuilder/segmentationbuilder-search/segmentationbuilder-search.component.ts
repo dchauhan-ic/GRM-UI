ce4e76f3b3863494fce557c8328a4aa4544dcdc1
djchauhan51435 <djchauhan51435@gmail.com>
2018-09-26 14:37:09 +0530

    GuestManger-StaticList-Angular6

--- GuestManager/src/app/gm/segmentationbuilder/segmentationbuilder-search/segmentationbuilder-search.component.ts
+++ GuestManager/src/app/gm/segmentationbuilder/segmentationbuilder-search/segmentationbuilder-search.component.ts
@@ -12,7 +12,29 @@ export class SegmentationbuilderSearchComponent implements OnInit {
 	selectedDashboard = "";
 	segmentName = undefined;
 	showDialog = false;
-	editSegmentId = undefined;
+  editSegmentId = undefined;
+  segments = [{
+    "count": 5,
+    "created": "09/08/2018",
+    "segmentId": 1,
+    "segmentName": "DJJSegment",
+    "successFlag": true,
+    "updated": "09/18/2018"
+  },{
+    "count": 7,
+    "created": "09/17/2018",
+    "segmentId": 2,
+    "segmentName": "TestSegment",
+    "successFlag": true,
+    "updated": "09/20/2018"
+  },{
+    "count": 11,
+    "created": "09/20/2018",
+    "segmentId": 3,
+    "segmentName": "test1234",
+    "successFlag": true,
+    "updated": "09/23/2018"
+  }];
   ngOnInit() {
   }
 
